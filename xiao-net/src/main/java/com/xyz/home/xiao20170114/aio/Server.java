4ead2363a6aac6f9795aadcd8b17235c345b7a61
justxiaoyizhi <18702528592@163.com>
2017-01-14 23:38:27 +0800

    TODO: 实现AIO

---
+++ xiao-net/src/main/java/com/xyz/home/xiao20170114/aio/Server.java
@@ -0,0 +1,8 @@
+package com.xyz.home.xiao20170114.aio;
+
+/**
+ * Created by Xiao on 2017/1/14.
+ */
+// TODO: 实现AIO
+public class Server {
+}
---
+++ xiao-net/src/main/java/com/xyz/home/xiao20170114/aio/ServerHandler.java
@@ -0,0 +1,17 @@
+package com.xyz.home.xiao20170114.aio;
+
+import java.nio.channels.CompletionHandler;
+
+/**
+ * Created by Xiao on 2017/1/14.
+ */
+// TODO: 实现AIO
+public class ServerHandler implements CompletionHandler {
+    public void completed(Object result, Object attachment) {
+
+    }
+
+    public void failed(Throwable exc, Object attachment) {
+
+    }
+}
