a36c91d23a1dc15cf085b8d3d9cd8bf475c3e0f2
justxiaoyizhi <18702528592@163.com>
2017-03-13 18:45:42 +0800

    新增Integer的-128到127缓存

---
+++ xiao-question/src/main/java/com/xyz/company/xiao20170313/Main.java
@@ -0,0 +1,28 @@
+package com.xyz.company.xiao20170313;
+
+/**
+ * @author xiao
+ * @create 2017-03-13 18:39
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        /*int i = 120;
+        Integer a = new Integer(120);
+        Integer b = new Integer(120);
+        System.out.println(a == b);*/
+
+        Integer i = 120;
+        Integer a = new Integer(120);
+        Integer b = new Integer(120);
+        System.out.println(a == b);
+
+        /*Integer a = 120;
+        Integer b = 120;
+        System.out.println(a == b);*/
+
+        /*Integer a = 1200;
+        Integer b = 1200;
+        System.out.println(a == b);*/
+    }
+}
