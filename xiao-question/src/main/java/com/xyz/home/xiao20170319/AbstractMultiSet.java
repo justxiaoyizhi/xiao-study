f9af0e25eb7d6e1e50e4d9ce2c8079443b9b5114
justxiaoyizhi <18702528592@163.com>
2017-03-19 13:55:39 +0800

    新增子类对父类的重写方法取舍

---
+++ xiao-question/src/main/java/com/xyz/home/xiao20170319/AbstractMultiSet.java
@@ -0,0 +1,11 @@
+package com.xyz.home.xiao20170319;
+
+/**
+ * Created by Xiao on 2017/3/19.
+ */
+public class AbstractMultiSet {
+
+    public void add() {
+        System.out.println("AbstractMultiSet");
+    }
+}
---
+++ xiao-question/src/main/java/com/xyz/home/xiao20170319/HashMultiSet.java
@@ -0,0 +1,12 @@
+package com.xyz.home.xiao20170319;
+
+/**
+ * Created by Xiao on 2017/3/19.
+ */
+public class HashMultiSet extends AbstractMapBasedMultiSet {
+
+    public static void main(String[] args) {
+        HashMultiSet set = new HashMultiSet();
+        set.add();
+    }
+}
