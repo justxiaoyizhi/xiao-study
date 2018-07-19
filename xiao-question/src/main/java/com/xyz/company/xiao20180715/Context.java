783f54720978dab828442bf6edb4888d94fa1269
xiaoyizhi <18702528592@163.com>
2018-07-19 10:13:10 +0800

    整理注释

--- xiao-question/src/main/java/com/xyz/company/xiao20180715/Context.java
+++ xiao-question/src/main/java/com/xyz/company/xiao20180715/Context.java
@@ -2,10 +2,19 @@ package com.xyz.company.xiao20180715;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Description: 上下文
+ * <p>
+ * PackageName: com.xyz.company.xiao20180715
+ * FileName: Context.java
+ * Copyright: Copyright (c)19/0. songxiaocai
+ *
+ * @author: xyz
+ * @create: 19/07/2018
+ */
 public class Context {
 
     /**
@@ -23,36 +32,65 @@ public class Context {
         return cards.length;
     }
 
-    public List<Card> washCard(int times) {
-        List<Card> cards = Lists.newArrayList(wash(this.cards, times));
-        System.out.println(cards);
-        return cards;
+    /**
+     * description: 洗牌
+     *
+     * @param times 洗牌的次数
+     * @return com.xyz.company.xiao20180715.Card[]
+     * @author: xyz
+     * @create: 15/07/2018
+     */
+    public Card[] washCard(int times) {
+        return wash(this.cards, times);
     }
 
+    /**
+     * description:
+     *
+     * @param cards
+     * @param times
+     * @return com.xyz.company.xiao20180715.Card[]
+     * @author: xyz
+     * @create: 19/07/2018
+     */
     private Card[] wash(Card[] cards, int times) {
         if (times == 0) {
             return cards;
         }
         int part = getCardLength() / 2;
 
-        Card[] partCard = getPartCards(cards, part);
-        Card[] otherPartCard = getOtherPartCards(cards, part);
+        Card[] partCard = getPartCards(cards, part, 0);
+        Card[] otherPartCard = getPartCards(cards, getCardLength() - part, part);
         Card[] cross = cross(partCard, otherPartCard);
         return wash(cross, times - 1);
     }
 
-    private Card[] getOtherPartCards(Card[] cards, int otherPart) {
-        Card[] otherPartCard = new Card[otherPart];
-        System.arraycopy(cards, cards.length - otherPart, otherPartCard, 0, otherPart);
-        return otherPartCard;
-    }
 
-    private Card[] getPartCards(Card[] cards, int part) {
-        Card[] partCard = new Card[part];
-        System.arraycopy(cards, 0, partCard, 0, part);
+    /**
+     * description: 将一副牌以{startPosition}为位置，partSize大小复制到一副新的牌
+     *
+     * @param cards         原来的牌
+     * @param partSize      新的牌组大小
+     * @param startPosition 新的牌大小
+     * @return com.xyz.company.xiao20180715.Card[]
+     * @author: xyz
+     * @create: 19/07/2018
+     */
+    private Card[] getPartCards(Card[] cards, int partSize, int startPosition) {
+        Card[] partCard = new Card[partSize];
+        System.arraycopy(cards, startPosition, partCard, 0, partSize);
         return partCard;
     }
 
+    /**
+     * description: 牌交叉合并
+     *
+     * @param partCard      一部分的牌
+     * @param otherPartCard 另一部分的牌
+     * @return com.xyz.company.xiao20180715.Card[]
+     * @author: xyz
+     * @create: 19/07/2018
+     */
     private Card[] cross(Card[] partCard, Card[] otherPartCard) {
 
         Card[] result = new Card[getCardLength()];
@@ -65,7 +103,9 @@ public class Context {
         Boolean flag = new Random().nextBoolean();
         for (Integer randomNum : randomNumList) {
 
+            // 交替进行
             if (flag) {
+                // 如果超过数组大小了，抹去超过的数值
                 if (otherExistBot + randomNum > otherPartCard.length) {
                     randomNum = otherPartCard.length - otherExistBot;
                 }
@@ -86,6 +126,13 @@ public class Context {
         return result;
     }
 
+    /**
+     * description: 获取随机数列表
+     *
+     * @return java.util.List<java.lang.Integer>
+     * @author: xyz
+     * @create: 19/07/2018
+     */
     private List<Integer> genRandomNumList() {
         Random random = new Random();
         List<Integer> randomNumList = Lists.newArrayList();
--- xiao-question/src/main/java/com/xyz/company/xiao20180715/Main.java
+++ xiao-question/src/main/java/com/xyz/company/xiao20180715/Main.java
@@ -1,10 +1,16 @@
 package com.xyz.company.xiao20180715;
 
+import com.google.common.collect.Lists;
+
 public class Main {
 
     public static void main(String[] args) {
-        Context context = new Context(Factory.initCard());
-        context.washCard(3);
+        Card[] initCards = Factory.initCard();
+        System.out.println("初始化之前的牌组:" + Lists.newArrayList(initCards));
+        Context context = new Context(initCards);
+
+        Card[] cards = context.washCard(3);
+        System.out.println("洗牌后的牌组:" + Lists.newArrayList(cards));
     }
 
 }
