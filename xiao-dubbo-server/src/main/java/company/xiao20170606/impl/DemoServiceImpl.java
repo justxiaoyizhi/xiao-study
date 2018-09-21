e05bd979548f91c2a11ac90ae19fd59719edf3a2
xiaoyizhi <18702528592@163.com>
2018-09-21 18:52:59 +0800

    :fix) 测试dubbo泛化调用，异常是自定义的

--- xiao-dubbo-server/src/main/java/company/xiao20170606/impl/DemoServiceImpl.java
+++ xiao-dubbo-server/src/main/java/company/xiao20170606/impl/DemoServiceImpl.java
@@ -2,6 +2,7 @@ package company.xiao20170606.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import company.xiao20170606.DemoService;
+import company.xiao20180921.CustomException;
 
 /**
  * Created by xiaoyizhi on 2017/6/6.
@@ -10,7 +11,7 @@ import company.xiao20170606.DemoService;
 public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
-        throw new ClassCastException("not cast");
+        throw new CustomException("not cast");
 //        return "hello " + name + ", i'm dubbo";
     }
 }
---
+++ xiao-dubbo-server/src/main/java/company/xiao20180921/CustomException.java
@@ -0,0 +1,16 @@
+package company.xiao20180921;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException() {
+        super();
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
